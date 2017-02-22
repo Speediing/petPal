02dd027be77610288e5a2c096f2d1bc08248afda
Speediing <jason.wiker@gmail.com>
2017-02-21 20:12:18 -0700

    initial storyboards

--- petPal/Dog.swift
+++ petPal/Dog.swift
@@ -1,34 +1,54 @@
 //
-//  ViewController.swift
+//  DogHealth.swift
 //  petPal
 //
 //  Created by Jason Wiker on 2017-02-21.
 //  Copyright © 2017 jason. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class Dog{
+var oftenToFeed = 1
     
+    var fed = 0
+    var walked = 0
+    var played = 0
+    var health = 0.0
     
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func getTimeDelta() -> Double{
         let currentDate = Date()
         let calendar = Calendar.current
         let midnightTime = DateComponents(year: (calendar.component(.year, from: currentDate) ), month: (calendar.component(.month, from: currentDate)), day: (calendar.component(.day, from: currentDate)) , hour: 0, second: 0)
         let midnightDate = calendar.date(from: midnightTime)!
         var date = currentDate.timeIntervalSince(midnightDate)
-        date = 1 - (date / 86400)
-        print(date)
+        let delta = 1 - (date / 86400)
+        return delta
+    }
+    
+    
+    
+    func checkStates(){
+       let statesList = [fed, walked, played]
+        for i in statesList{
+            if i == 1{
+             health += 33.3
+            }
+        }
+        if health > 100 {
+        health = 100
+        }
         
+
         
         
     }
-
+    func getExcersise(){
+        
+    }
+    func getHappiness(){
+        
+    }
     
-
-
+    
 }
-
